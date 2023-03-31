4e77a3ce052fb12dd2aeccde0b564a9e819688c0
Siddharth <Siddharth@gitlab.novaphaze.com>
2023-03-30 21:57:59 -0400

    ????

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import { Command } from '@tauri-apps/api/shell'
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
 async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
+  // if (greetMsgEl && greetInputEl) {
+  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  //   greetMsgEl.textContent = await invoke("greet", {
+  //     name: greetInputEl.value,
+  //   });
+  // }
+
+  const command = new Command("echo $PATH").execute()
+  greetMsgEl.textContent = (await command).stdout;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
