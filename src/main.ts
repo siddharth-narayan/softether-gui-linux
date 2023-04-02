d42ef8bd9469532371e481367765d2b64bf2baf3
Siddharth <Siddharth@gitlab.novaphaze.com>
2023-04-02 17:09:50 -0400

    So i did some stuff but I don't know if its good

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,28 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { Command } from '@tauri-apps/api/shell'
+import {  } from  '@tauri-apps/api/path'
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
+let command: Command
 
 async function greet() {
-  // if (greetMsgEl && greetInputEl) {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   greetMsgEl.textContent = await invoke("greet", {
-  //     name: greetInputEl.value,
-  //   });
-  // }
+  
 
-  const command = new Command("echo $PATH").execute()
-  greetMsgEl.textContent = (await command).stdout;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
+  
   document
     .querySelector("#greet-button")
     ?.addEventListener("click", () => greet());
+  
+  command = new Command("sh")
+  command.spawn()
+  command.stdout.on('data', function(line){
+    greetMsgEl.textContent = line
+    console.log(line);
+  })
 });
