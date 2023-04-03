0a2cc9c8fda134b96f21269d6a73070b9db98fcd
Siddharth <siddharth.060605@gmail.com>
2023-04-03 15:11:42 -0400

    Got the shell working and now trying to do the ux

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { invoke } from "@tauri-apps/api/tauri";
-import { Command } from '@tauri-apps/api/shell'
+import { Child, Command } from '@tauri-apps/api/shell'
 import {  } from  '@tauri-apps/api/path'
+import { BaseDirectory } from "@tauri-apps/api/path";
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
-let command: Command
+let process: Child
+
+const command = new Command("sh")
 
 async function greet() {
-  
-
+  process.write(greetInputEl.value + "\n")
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   
@@ -19,8 +21,8 @@ window.addEventListener("DOMContentLoaded", () => {
     .querySelector("#greet-button")
     ?.addEventListener("click", () => greet());
   
-  command = new Command("sh")
-  command.spawn()
+  
+  process = await command.spawn()
   command.stdout.on('data', function(line){
     greetMsgEl.textContent = line
     console.log(line);
