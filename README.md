27579553062159ca5b861bc88986c8b86a2d9626
Siddharth <Siddharth@gitlab.novaphaze.com>
2023-04-06 07:37:05 -0400

    Worked on UI, scripting. VPN Setup now works

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Tauri + Vanilla TS
+# SoftEther GUI for Linux
 
-This template should help get you started developing with Tauri in vanilla HTML, CSS and Typescript.
+I make 0 guarantees that this program works correctly. I'm just a person who decided to make a softether gui on linux because there didnt seem to already be one.
 
-## Recommended IDE Setup
+## Setup
 
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+git clone this repo, and then install the tauri dependencies.
+then run ```npm run tauri build```
--- src/main.ts
+++ src/main.ts
@@ -2,40 +2,109 @@
 import { Child, Command } from '@tauri-apps/api/shell'
 import { BaseDirectory, exists, readTextFile, writeTextFile } from  '@tauri-apps/api/fs'
 import { appDataDir } from "@tauri-apps/api/path";
+import { Arch, arch, Platform, platform } from '@tauri-apps/api/os'
 
 let sh: Child;
 let terminalElement: HTMLElement;
-
-const command = new Command("sh")
 
 async function readTextFileFromAppData(path: string): Promise<String> {
   return readTextFile(path, {dir: BaseDirectory.AppData})
 }
 
+
 function shwrite(line: string){
   sh.write(line + "\n")
   console.log("Wrote to shell: " + line)
+  writeTerm("$ " + line)
 }
 
-function writeLineToTerminal(line: string){
+function writeTerm(line: string){
   let htmlLine = document.createElement("nu-block")
   htmlLine.append(document.createTextNode(line))
   terminalElement.appendChild(htmlLine)
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
+function intitialize(plat: Platform, arch: Arch, appDataDirPath: String){
+  let awk = "awk '/vpnclient/"
 
-  terminalElement = document.getElementById('cmd')!
-  sh = await command.spawn()
-  command.stdout.on('data', function(line){
-    writeLineToTerminal(line)
-  })
-  
-  shwrite("ls")
-
-  if(!await exists("logins.json", { dir: BaseDirectory.AppData })){
-    shwrite("cp assets/blank.json " + await appDataDir() + "config.json")
+  switch(arch) {
+    case 'aarch64':
+      awk += " && /arm64-64bit/"
+      break
+    case 'arm':
+      awk += " && /arm-32bit/"
+      break
+    case 'mips':
+      awk += " && /mips/"
+      break
+    case 'mips64':
+      awk += " && /mips/"
+      break
+    case 'powerpc':
+      awk += " && /powerpc-32bit/"
+      break
+    case 'powerpc64':
+      awk += " && /powerpc64-64bit/"
+      break
+    case 'sparc64':
+      awk += " && /sparc64-64bit/"
+      break
+    case 'x86':
+      awk += " && /x86-32bit/"
+      break
+    case 'x86_64':
+      awk += " && /x64-64bit/"
+      break
+    default:
+      awk += " && /x64-64bit/"
+      break
   }
 
+  switch(plat){
+    case 'darwin':
+      awk += " && /macos/"
+      break
+    case 'freebsd':
+      awk += " && /freebsd/"
+      break
+    case 'linux':
+      awk += " && /linux/"
+      break
+    case 'solaris':
+      awk += " && /solaris/"
+      break
+    default: 
+      awk += " && /linux/"
+      break
+  }
+
+  shwrite("cp assets/blank.json " + appDataDirPath + "config.json")
+  shwrite("cd " + appDataDirPath)
+  shwrite("wget $(curl -s https://api.github.com/repos/SoftEtherVPN/SoftEtherVPN_Stable/releases/latest | grep 'browser_' | cut -d\\\" -f4 | " + awk + "')")
+  shwrite("gzip -d $(ls | grep soft | cut -d ' ' -f9)")
+  shwrite("tar -xvf $(ls | grep soft | cut -d ' ' -f9)")
+  shwrite("rm $(ls | grep soft | cut -d ' ' -f9)")
+  shwrite("cd ./vpnclient/")
+  shwrite("cat $(ls | cut -d ' ' -f9 | awk '/License/ && /en/')")
+  shwrite("make main")
+
+}
+
+
+
+window.addEventListener("DOMContentLoaded",  async () => {
+
+  terminalElement = document.getElementById('cmd')!
+
+  let command = new Command("sh")
+  sh = await command.spawn().then()
+  command.stdout.on('data', function(line){
+    writeTerm(line)
+  })
   
+  if(!await exists("config.json", { dir: BaseDirectory.AppData })){
+    console.log("intitializing")
+    intitialize(await platform(), await arch(), await appDataDir())
+  }
 });
+
